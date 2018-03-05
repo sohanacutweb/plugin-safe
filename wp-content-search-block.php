1cefcc356ad6fa9798689380c31503157d692bbf
sohanacutweb <sohan.acutweb@gmail.com>
2018-03-05 11:34:53 +0530

    plugin start

---
+++ wp-content-search-block.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Content Search Block
+Description: o block search engine bots, specifically, Google Bot from indexing/scanning the wp-content (wp-themes & wp-plugins) folder of Wordpress.
+Version: 1.0.0
+Author: Acutweb
+Author URI: http://acutweb.com
+*/
+?>
