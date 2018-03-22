6439eab1126ab90170842f2f7eaa066c8e7244e1
sohanacutweb <sohan.acutweb@gmail.com>
2018-03-22 10:35:09 +0530

    Adding Menu out from under setting

--- plugin-safe.php
+++ plugin-safe.php
@@ -49,12 +49,13 @@ add_filter('init', 'wpsecure_init_locale');
 /**
  * Add the WpSecure menu to the Settings menu
  */
+ 
+ add_action('admin_menu', 'wpsecure_admin_menu');
+ 
 function wpsecure_admin_menu() {
-	add_options_page('WP Content Search Block', 'WP Content Search Block', 8, 'wp-csb-directory', 'wpcsb_submenu');
+	add_menu_page('WP Content Search Block', 'WP Content Search Block', 'manage_options', 'wp-csb-directory', 'wpcsb_submenu', 'dashicons-lock');
 	
-	 //add_menu_page( 'WP Content Search Block', 'WP Content Search Block', 'manage_options', 'wp-csb-directory', 'wpcsb_submenu'); 
 }
-add_action('admin_menu', 'wpsecure_admin_menu');
 
 function wpsecure_write_htaccess($tre){
 	global $siteUrl;
@@ -100,4 +101,78 @@ function wpsecure_write_htaccess($tre){
 function wpcsb_submenu() {
 	include("include/common_function.php");
 	}
+/**
+ * Plugin Update Checker
+ */
+$api_url = 'http://acutweb.com/plugin-safe-api/index.php/';
+$plugin_slug = basename(dirname(__FILE__));
+
+// Take over the update check
+add_filter('pre_set_site_transient_update_plugins', 'check_for_plugin_update');
+
+function check_for_plugin_update($checked_data) {
+	global $api_url, $plugin_slug;
+	//Comment out these two lines during testing.
+	if (empty($checked_data->checked))
+		return $checked_data;
+	
+	$args = array(
+		'slug' => $plugin_slug,
+		'version' => $checked_data->checked[$plugin_slug .'/'. $plugin_slug .'.php'],
+	);
+	$request_string = array(
+			'body' => array(
+				'action' => 'basic_check', 
+				'request' => serialize($args),
+				'api-key' => md5(get_bloginfo('url'))
+			),
+			'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
+		);
+	
+	// Start checking for an update
+	$raw_response = wp_remote_post($api_url, $request_string);
+	if (!is_wp_error($raw_response) && ($raw_response['response']['code'] == 200))
+		$response = unserialize($raw_response['body']);
+	
+	if (is_object($response) && !empty($response)) // Feed the update data into WP updater
+		$checked_data->response[$plugin_slug .'/'. $plugin_slug .'.php'] = $response;
+	
+	return $checked_data;
+}
+
+// Take over the Plugin info screen
+add_filter('plugins_api', 'plugin_api_call', 10, 3);
+
+function plugin_api_call($def, $action, $args) {
+	global $plugin_slug, $api_url;
+	
+	if ($args->slug != $plugin_slug)
+		return false;
+	
+	// Get the current version
+	$plugin_info = get_site_transient('update_plugins');
+	$current_version = $plugin_info->checked[$plugin_slug .'/'. $plugin_slug .'.php'];
+	$args->version = $current_version;
+	
+	$request_string = array(
+			'body' => array(
+				'action' => $action, 
+				'request' => serialize($args),
+				'api-key' => md5(get_bloginfo('url'))
+			),
+			'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
+		);
+	
+	$request = wp_remote_post($api_url, $request_string);
+	if (is_wp_error($request)) {
+		$res = new WP_Error('plugins_api_failed', __('An Unexpected HTTP Error occurred during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message());
+	} else {
+		$res = unserialize($request['body']);
+		if ($res === false)
+			$res = new WP_Error('plugins_api_failed', __('An unknown error occurred'), $request['body']);
+	}
+	return $res;
+}
+
+// End Plugin Update Checker
 ?>
